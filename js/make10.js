36c367faa66540a4d28c5baf21425c52278e6e6a
ajimu-takuha <94386997+ajimu-takuha@users.noreply.github.com>
2023-05-25 20:15:34 +0900

    Add files via upload

--- js/make10.js
+++ js/make10.js
@@ -3,8 +3,8 @@
 
 function make10(nums, target) {
 
-  var 数字の並び替えパターンA = {};
-  var 数字の並び替えパターンB = {};
+  var patternA = {};
+  var patternB = {};
   // if ($("#数字の並び替え").val() == "していい") {
   // 加減算混在もしくは剰余算混在の場合の数字の並び替えパターンの作成
   for (var i = 0; i < 4; i++) {
@@ -14,7 +14,7 @@ function make10(nums, target) {
     var v2 = nums_.shift();
     var v3 = nums_.shift();
     var v4 = nums_.shift();
-    数字の並び替えパターンA[[v1, v2, v3, v4].join(".")] = [v1, v2, v3, v4];
+    patternA[[v1, v2, v3, v4].join(".")] = [v1, v2, v3, v4];
   }
   // 四則演算混在の場合の数字の並び替えパターンの作成
   for (var i = 0; i < 4; i++) {
@@ -25,38 +25,38 @@ function make10(nums, target) {
         var v2 = nums_[j]; nums_.splice(j, 1);
         var v3 = nums_[k]; nums_.splice(k, 1);
         var v4 = nums_[0];
-        数字の並び替えパターンB[[v1, v2, v3, v4].join(".")] = [v1, v2, v3, v4];
+        patternB[[v1, v2, v3, v4].join(".")] = [v1, v2, v3, v4];
       }
     }
   }
   // } else {
-  //   数字の並び替えパターンA[nums.join(".")] = nums;
-  //   数字の並び替えパターンB[nums.join(".")] = nums;
+  //   patternA[nums.join(".")] = nums;
+  //   patternB[nums.join(".")] = nums;
   // }
   var exps = {};
 
   // すべて加算の場合：素直に入力順に計算
   // exps[nums.join("+")] = null;
   // 加減算混合の場合：カッコは関係ない。最初の文字のみ考慮する。
-  var opes = ["+", "-"];
-  for (var nm_ in 数字の並び替えパターンA) {
-    var nm = 数字の並び替えパターンA[nm_];
-    var v1 = nm[0];
-    var v2 = nm[1];
-    var v3 = nm[2];
-    var v4 = nm[3];
-    for (var j = 1; j < 8; j++) {
-      var exp = v1 + opes[j & 1] + v2 + opes[(j & 2) / 2] + v3 + opes[(j & 4) / 4] + v4;
-      exps[exp] = null;
-    }
-  }
+  // var opes = ["+", "-"];
+  // for (var nm_ in patternA) {
+  //   var nm = patternA[nm_];
+  //   var v1 = nm[0];
+  //   var v2 = nm[1];
+  //   var v3 = nm[2];
+  //   var v4 = nm[3];
+  //   for (var j = 1; j < 8; j++) {
+  //     var exp = v1 + opes[j & 1] + v2 + opes[(j & 2) / 2] + v3 + opes[(j & 4) / 4] + v4;
+  //     exps[exp] = null;
+  //   }
+  // }
 
   // すべて乗算の場合：素直に入力順に計算
-  exps[nums.join("*")] = null;
+  // exps[nums.join("*")] = null;
   // 乗除算混合の場合：カッコは関係ない。最初の文字のみ考慮する。
   var opes = ["*", "/"];
-  for (var nm_ in 数字の並び替えパターンA) {
-    var nm = 数字の並び替えパターンA[nm_];
+  for (var nm_ in patternA) {
+    var nm = patternA[nm_];
     var v1 = nm[0];
     var v2 = nm[1];
     var v3 = nm[2];
@@ -78,8 +78,8 @@ function make10(nums, target) {
     ["/", "-", "-"]
   ];
 
-  for (var nm_ in 数字の並び替えパターンB) {
-    var nm = 数字の並び替えパターンB[nm_];
+  for (var nm_ in patternB) {
+    var nm = patternB[nm_];
     var v1 = nm[0];
     var v2 = nm[1];
     var v3 = nm[2];
@@ -146,8 +146,8 @@ function make10(nums, target) {
     ["-", "/", "/"]
   ];
 
-  for (var nm_ in 数字の並び替えパターンB) {
-    var nm = 数字の並び替えパターンB[nm_];
+  for (var nm_ in patternB) {
+    var nm = patternB[nm_];
     var v1 = nm[0];
     var v2 = nm[1];
     var v3 = nm[2];
@@ -198,7 +198,7 @@ function make10(nums, target) {
   for (var exp in exps) {
     exp = "return " + exp;
     if (Function(exp)() == target) {
-      // console.log(exp + "で成功");
+      // console.log(exp);
       // console.log(target + "に計算成功");
       return [true, exp];
     }
